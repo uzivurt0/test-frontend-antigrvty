fa61dac03dce6dc278f4a4d2fe3ba07057640d5d
Muhammad Faisal Farhan <gue.fafa00@gmail.com>
2021-09-21 13:49:43 +0700

    fixing route v14

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
         <Header />
             <Switch>        
                 <Route path={process.env.PUBLIC_URL + '/'} exact component ={Dealer}/>
-                <Route path={process.env.PUBLIC_URL + '/outlet'} component={Outlet}/>
-                <Route path={process.env.PUBLIC_URL + '/XL7'} component={Catalogue}/>       
+                <Route path={'/outlet'} component={Outlet}/>
+                <Route path={'/XL7'} component={Catalogue}/>       
             </Switch>
           <Footer />
       {/* <RouterView /> */}
